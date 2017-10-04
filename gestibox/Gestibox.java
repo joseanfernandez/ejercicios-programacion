9ff18a92c10b66b5e54d17dd40c660e81f3653d4
Jose Antonio Fernández García <fernanj40@gmail.com>
2017-10-04 10:54:04 +0200

    Rama con proyecto personal

---
+++ gestibox/Gestibox.java
@@ -0,0 +1,7 @@
+public class Gestibox { // Clase principal
+  public static void main(String[] args) {
+    System.out.println("G E S T I B O X");
+    System.out.println("1. Heros");
+    System.out.println("2. Girls");
+  }
+} 
