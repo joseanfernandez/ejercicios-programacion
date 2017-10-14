0068a74f52206e72e21599ab347ae3a1f51ff79c
Jose Antonio Fernández García <fernanj40@gmail.com>
2017-10-14 12:10:07 +0200

    Subido ejercicio 6 del tema 4

---
+++ Tema4/Ejercicio06.java
@@ -0,0 +1,34 @@
+/**
+*Tema 4*
+*Ejercicio 6
+*Programa que calcule el tiempo que tardará en caer un objeto desde una altura h.
+*/
+
+import java.util.Scanner;
+
+public class Ejercicio06 { // Clase principal
+  public static void main(String[] args) {
+  
+  Scanner s = new Scanner(System.in);
+  System.out.println("Calcular el tiempo que tarda en caer un objeto");
+  System.out.print("Introduce la altura desde la que cae en metros: ");
+  int h = s.nextInt();
+  
+  if (h < 0) {
+      System.out.print("Error, no es posible que la altura sea negativa"); 
+    } 
+  if (h == 0) {
+      System.out.println("Error, no es posible que la altura sea cero");
+    }    
+  
+  if (h > 0) {
+    double t = Math.sqrt((2*h)/9.81);
+      System.out.println("El tiempo que tarda en caer el objeto desde " + h + "m es: " + t+ "s");
+    }  
+  
+
+  }
+} 
+
+  
+  
