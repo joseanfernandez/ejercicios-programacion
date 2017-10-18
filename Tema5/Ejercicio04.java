e9737adb08cae6ec2f1c35748a90c0383fe664ab
Jose Antonio Fernández García <fernanj40@gmail.com>
2017-10-18 08:47:33 +0200

    Subido ejercicio 4 del tema 5

---
+++ Tema5/Ejercicio04.java
@@ -0,0 +1,20 @@
+/**
+*Tema 5*
+*Ejercicio 4
+*Muestra los números del 320 al 160, contando de 20 en 20 hacia atrás
+*utilizando un bucle for .
+*/
+
+import java.util.Scanner;
+
+public class Ejercicio04 { // Clase principal
+  public static void main(String[] args) {
+    
+    System.out.println("Números del 320 al 160, de 20 en 20: ");
+    
+    for (int i=320;i>=160;i=i-20) {
+      System.out.println(i);
+      
+    }
+  } 
+}
