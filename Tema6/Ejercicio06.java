eead8799428e7677de05f916276c705d643f803c
Jose Antonio Fernández García <fernanj40@gmail.com>
2017-11-13 11:08:59 +0100

    Subido ejercicio 6 del tema 6

---
+++ Tema6/Ejercicio06.java
@@ -0,0 +1,44 @@
+/**
+*Tema 6*
+*Ejercicio 6
+*Escribe un programa que piense un número al azar entre 0 y 100. 
+*El usuario debe adivinarlo y tiene para ello 5 oportunidades. 
+*Después de cada intento fallido, el programa dirá cuántas oportunidades 
+*quedan y si el número introducido es menor o mayor que el número secreto.
+*/
+
+import java.util.Scanner;
+
+public class Ejercicio06 { // Clase principal
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    System.out.println("Adivina el número que estoy pensando.");
+    System.out.println("Tienes 5 intentos.");
+    System.out.print("Introduce un número: ");
+    int numero = 0;
+    int numsecreto = (int) (Math.random() * 101);
+    int intentos = 5;
+    System.out.println(numsecreto);
+    
+    do {
+      numero = s.nextInt();
+      
+      if (numero == numsecreto) {
+        System.out.println("Enhorabuena, has acertado.");
+      }
+      if (numero < numsecreto) {
+        System.out.println("Lo siento, el número que estoy pensando es mayor.");
+      }
+      if (numero > numsecreto) {
+        System.out.println("Lo siento, el número que estoy pensando es menor.");
+      }
+      intentos --;
+      
+    }  while ((numero != numsecreto) && (intentos > 0)); 
+    
+    if (intentos == 0) {
+        System.out.print("Has agotado los 5 intentos");
+      }   
+    
+  } 
+}
