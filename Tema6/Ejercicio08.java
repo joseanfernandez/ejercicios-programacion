5308424cc8de2936737cd74e12b2fa9116afd045
Jose Antonio Fernández García <fernanj40@gmail.com>
2017-11-13 19:39:25 +0100

    Subido ejercicio 8 del tema 6

---
+++ Tema6/Ejercicio08.java
@@ -0,0 +1,64 @@
+/**
+*Tema 6*
+*Ejercicio 8
+*Modifica el programa anterior para que la probabilidad de que salga un 1 sea de 1/2,
+*la probabilidad de que salga x sea de 1/3 y la probabilidad de que salga 2 sea de 1/6.
+*Pista: 1/2 = 3/6 y 1/3 = 2/6.
+*/
+
+public class Ejercicio08 { // Clase principal
+  public static void main(String[] args) {
+    
+    for (int i = 0; i < 14; i++) {
+      int resultado = (int) (Math.random() * 6);
+      System.out.println("┌─┬─┬─┐");
+      switch (resultado) {
+        case 0:
+          System.out.println("│ │X│ │");
+          break;
+        case 1:
+          System.out.println("│ │X│ │");
+          break;
+        case 2:
+          System.out.println("│1│ │ │");
+          break;
+        case 3:
+          System.out.println("│1│ │ │");
+          break;
+        case 4:
+          System.out.println("│1│ │ │");
+          break;
+        case 5:
+          System.out.println("│ │ │2│");
+          break;
+        default:
+      }
+      System.out.println("└─┴─┴─┘");
+    }
+      
+       int pleno = (int) (Math.random() * 6);
+      System.out.println("            ┌─┬─┬─┐");
+      switch (pleno) {
+        case 0:
+          System.out.println("Pleno al 15 │ │X│ │");
+          break;
+        case 1:
+          System.out.println("Pleno al 15 │ │X│ │");
+          break;
+        case 2:
+          System.out.println("Pleno al 15 │1│ │ │");
+          break;
+        case 3:
+          System.out.println("Pleno al 15 │1│ │ │");
+          break;
+        case 4:
+          System.out.println("Pleno al 15 │1│ │ │");
+          break;
+        case 5:
+          System.out.println("Pleno al 15 │ │ │2│");
+          break;
+        default:
+      }
+      System.out.println("            └─┴─┴─┘");
+  }
+}
